e97f9c7418a1bb345dd59e20e7482627b9b6df9f
DaveOlivae <davioliveiramelo2604@gmail.com>
2025-12-31 22:13:29 -0300

    functions

--- spinning_cube.c
+++ spinning_cube.c
@@ -56,24 +56,34 @@ void print_screen()
     }
 }
 
+Vec rotate_y(float x0, float y0, float z0)
+{
+    // rotates the cube along the y axis
+    // new x, y and z values
+    float x = x0*cos(angle) + sin(angle) * z0; 
+    float y = y0; 
+    float z = -sin(angle) * x0 + cos(angle) * z0;
+
+    Vec rotated_vec = {x, y, z};
+    
+    return rotated_vec;
+}
+
 int main()
 {
-    int number_of_points = 12;
+    int number_of_points = 8;
 
     // so, right now the cube is merely a list of points, each with z y z coordinates
     Vec cube[] =
     {
-        {0, 0, 0},      // upper right vertex
-        {0, 0, 1},     // lower right vertex
-        {1, 0, 0},     // upper left vertex
-        {1, 0, 1},    // lower left vertex
-        {0, 1, 0},     // upper right vertex (back)
-        {0.06, 1, 0},
-        {0.12, 1, 0},
-        {0.18, 1, 0},
-        {0, 1, 1},    // lower right vertex (back)
-        {1, 1, 0},    // upper left vertex (back)
-        {1, 1, 1},   // lower left vertex (back)
+        {0, 0, 0},      // p1: upper right vertex
+        {0, 0, 1},      // p2: lower right vertex
+        {1, 0, 0},      // p3: upper left vertex
+        {1, 0, 1},      // p4: lower left vertex
+        {0, 1, 0},      // p5: upper right vertex (back)
+        {0, 1, 1},      // p6: lower right vertex (back)
+        {1, 1, 0},      // p7: upper left vertex (back)
+        {1, 1, 1},      // p8: lower left vertex (back)
         
     };
 
@@ -89,21 +99,12 @@ int main()
         // here we're gonna have to project a 3d coordinate in a 2d plane
         for (int i = 0; i < number_of_points; i++)
         {
-            // original x, y and z values
-            float x0 = cube[i].x;
-            float y0 = cube[i].y;
-            float z0 = cube[i].z;
+            Vec rotated_vec = rotate_y(cube[i].x, cube[i].y, cube[i].z);
 
-            // rotates the cube along the y axis
-                // new x, y and z values
-            float x = x0*cos(angle) + sin(angle) * z0; 
-            float y = y0; 
-            float z = -sin(angle) * x0 + cos(angle) * z0;
+            rotated_vec.z += DIST;
 
-            z += DIST;
-
-            int sx = (int)(x / z * SCALE + WIDTH/2);
-            int sy = (int)(-y / z * SCALE + HEIGHT/2);
+            int sx = (int)(rotated_vec.x / rotated_vec.z * SCALE + WIDTH/2.0);
+            int sy = (int)(-rotated_vec.y / rotated_vec.z * SCALE + HEIGHT/2.0);
 
             if (sx >= 0 && sx < WIDTH && sy >= 0 && sy < HEIGHT)
                 screen[sy][sx] = '*';
